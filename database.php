3a5c4f741f4854f8cd8a423118ccce6a7d1913cf
ADITYA PRATAP SINGH <68942781+Aditya-cyber007@users.noreply.github.com>
2022-02-02 15:24:34 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,16 @@
-<?php 
-
-$host="localhost";
-$user="root";
-$password="";
-$db="user";
-
-$conn = new mysqli($host, $user, $password, $db);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:adityapratap620.database.windows.net,1433; Database = user", "hospital", "Aditya@007");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-?>     
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "hospital", "pwd" => "Aditya@007", "Database" => "user", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:adityapratap620.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
