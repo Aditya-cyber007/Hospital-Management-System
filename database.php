91a2d2fce57d12783f49690686b34120474c8a80
ADITYA PRATAP SINGH <68942781+Aditya-cyber007@users.noreply.github.com>
2022-02-03 13:31:49 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:adityapratap620.database.windows.net,1433; Database = user", "hospital", "Aditya@007");
+    $conn = new PDO("sqlsrv:server = tcp:database1.database.windows.net,1433; Database = database1", "aditya123", "Aditya@007");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "hospital", "pwd" => "Aditya@007", "Database" => "user", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:adityapratap620.database.windows.net,1433";
+$connectionInfo = array("UID" => "aditya123", "pwd" => "Aditya@007", "Database" => "database1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:database1.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
